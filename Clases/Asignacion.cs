da2c20b72560737950e89463c266fcf208c2b1aa
Juan Castro <juanmarco.ct@gmail.com>
2024-10-07 16:26:57 -0600

    Cambios de nuevo

--- Clases/Asignacion.cs
+++ Clases/Asignacion.cs
@@ -8,6 +8,25 @@ namespace PruebaAlgoritmoSimulacion.Clases
 {
     public class Asignacion
     {
+        public double Latitud{ get; set;}
+        public double Longitud { get; set; }
 
+        public int IdPunto { get; set; }
+
+        public bool Activo { get; set; }
+
+        public string Especie { get; set; }
+
+        public Asignacion()
+        {
+        }
+        public Asignacion(Asignacion asignacion)
+        {
+            this.Latitud = asignacion.Latitud;
+            this.Longitud = asignacion.Longitud;
+            this.Activo = asignacion.Activo;
+            this.Especie = asignacion.Especie;
+            this.IdPunto = asignacion.IdPunto;
+        }
     }
 }
