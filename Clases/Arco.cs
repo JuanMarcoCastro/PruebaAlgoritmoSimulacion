03b44c5e8dd7d39e8245d6807e2352c371ebd43f
Juan Castro <juanmarco.ct@gmail.com>
2024-10-07 16:25:54 -0600

    Avance Clases

---
+++ Clases/Arco.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaAlgoritmoSimulacion.Clases
+{
+    public class Arco
+    {
+        // Nace Id Punto
+        public int IdPuntoOrigen { get; set; }
+        public int IdPuntoDestino { get; set; }
+        public double Distancia { get; set; }
+        
+        public double Competitividad { get; set; }
+
+        // Constructor
+        public Arco()
+        { 
+        }
+        public Arco(Arco arco)
+        {
+            this.IdPuntoOrigen = arco.IdPuntoOrigen;
+            this.IdPuntoDestino = arco.IdPuntoDestino;
+            this.Distancia = arco.Distancia;
+            this.Competitividad = arco.Competitividad;
+        }
+    }
+}
